009fed0735e85e5c56dce583ae5b5905564b0195
yehiaAboulela <yhia.themora@gmail.com>
2024-07-02 21:47:09 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const sideNavOverlay = document.querySelector(".overlay");
+const mobileNav = document.querySelector(".mobile-nav");
+const openSideNavBtn = document.querySelector("#openSideNavBtn");
+const escMinuBtn = document.querySelector("#escMinuBtn");
+
+openSideNavBtn.addEventListener("click", function () {
+  mobileNav.classList.add("active");
+});
+escMinuBtn.addEventListener("click", function () {
+  mobileNav.classList.remove("active");
+});
+sideNavOverlay.addEventListener("click", function () {
+  mobileNav.classList.remove("active");
+});
